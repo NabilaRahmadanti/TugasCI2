ca83fc3dcc943bdbd7cd74e3be734d3d02895b69
Your Name <you@example.com>
2019-01-22 14:29:01 +0700

    selesai

---
+++ application/views/event.php
@@ -0,0 +1,7 @@
+<div class="sale">
+    <img src="<?=base_url()?>assets/img/sale.png" alt="">
+</div>
+
+<div class="sale2">
+    <img src="<?=base_url()?>assets/img/sale 2.png" alt="">
+</div>
---
+++ application/views/v_profil.php
@@ -0,0 +1,14 @@
+
+    <div class="box">
+    <img style="width: 170px; border-radius: 10%; margin-top:-170px;" src="<?=base_url()?>assets/foto.png" alt="">
+    <h1>Hello!</h1>
+    <div id="body">
+            <p>Nama Lengkap          : Nabila Rahmadanti Noviasari</p>
+            <p>Nama Panggilan        : Nabila / Bila</p>
+            <p>Tempat, Tanggal lahir : Malang, 22 November 2001</p>
+            <p>Jenis Kelamin         : Perempuan</p>
+            <p>Agama                 : Islam</p>
+            <p>Alamat                : Jl. Sawojajar Gg. 19 No.50k, Malang</p>
+            <p>Sekolah               : SMK Telkom Malang</p>
+	</div>
+	</div>
